3b0a95ccc24bba5a78d275cc619234052371ed51
dinhujr <c.souza0804@gmail.com>
2015-01-10 17:25:19 -0200

    Main Window, initial commit

---
+++ src/br/ufrj/cos/expline/scicumulus/conversion/view/MainWindow.java
@@ -0,0 +1,43 @@
+package br.ufrj.cos.expline.scicumulus.conversion.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+
+public class MainWindow extends JFrame 
+{
+	public MainWindow()
+	{
+		super();
+		
+		this.setSize(400, 400);
+		
+		this.setLocationRelativeTo(null);
+		
+		this.setTitle("Scicumulus Instantiation");
+		
+		JTabbedPane jtp = new JTabbedPane();
+		//jtp.setSize(width, height);
+		this.add(jtp);
+		
+		JPanel aloha = new JPanel();
+		JPanel aloha1 = new JPanel();
+		
+		jtp.addTab("Claudio", aloha );
+		
+		jtp.addTab("Joao", aloha1 );
+		
+		JTextField jtf = new JTextField();
+		jtf.setText("Alo meu amigo");
+		
+		
+		
+		aloha.add(jtf);
+		
+		this.repaint();
+		
+		this.setVisible(true);
+		
+	}
+}
---
+++ src/br/ufrj/cos/expline/scicumulus/conversion/view/main.java
@@ -0,0 +1,11 @@
+package br.ufrj.cos.expline.scicumulus.conversion.view;
+
+import javax.swing.JFrame;
+
+public class main 
+{
+	public static void main(String[] args)
+	{
+		new MainWindow();
+	}
+}
